8f3d2c465c5010442b05e53be32d6d6c037203dd
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2020-10-19 12:28:36 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Fake News</title>
+</head>
+<body>
+
+
+
+<div class="container">
+<div class="news-item">
+    <div class="news-item-img">
+        <img src="images/news1.jpg" alt="">
+    </div>
+    <div class="news-item-info">
+        <div class="news-item-date">
+            <span>Monday</span>
+            <span>September 20 2020</span>
+        </div>
+        <h1 class="news-item-title">Kanye Does It Again</h1>
+        <p class="news-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                  Illum consequuntur blanditiis hic sint adipisci.
+                                  Eveniet reiciendis a aliquam neque! Incidunt blanditiis atque pariatur minus quibusdam, voluptas omnis. Nulla, earum deleniti!
+        </p>
+        <a href="#" class="news-item-cta">Read more >></a>
+    </div>
+</div>
+
+
+<div class="news-item">
+    <div class="news-item-img">
+        <img src="images/news2.jpg" alt="">
+    </div>
+    <div class="news-item-info">
+        <div class="news-item-date">
+            <span>Monday</span>
+            <span>September 20 2020</span>
+        </div>
+        <h1 class="news-item-title">Got Crabs?</h1>
+        <p class="news-item-text">This couple went crab-fishing, the crab they found will BLOW YOUR MIND.
+        </p>
+        <a href="#" class="news-item-cta">Read more >></a>
+    </div>
+</div>
+
+
+
+<div class="news-item">
+    <div class="news-item-img">
+        <img src="images/news3.jpg" alt="">
+    </div>
+    <div class="news-item-info">
+        <div class="news-item-date">
+            <span>Monday</span>
+            <span>September 20 2020</span>
+        </div>
+        <h1 class="news-item-title">Ten Life-hacks for parents.</h1>
+        <p class="news-item-text">They say early parenthood is a struggle, here are our best tips for finding peace and quiet where there usually is none.
+        </p>
+        <a href="#" class="news-item-cta">Read more >></a>
+    </div>
+</div>
+
+
+<div class="news-item">
+    <div class="news-item-img">
+        <img src="images/news4.jpg" alt="">
+    </div>
+    <div class="news-item-info">
+        <div class="news-item-date">
+            <span>Monday</span>
+            <span>September 20 2020</span>
+        </div>
+        <h1 class="news-item-title">Watch this not-soon-to-be-wife's 'ringing' endorsement.</h1>
+        <p class="news-item-text">How much did this guy pay for this ring? Let's just say we hope that he kept the receipt..
+        </p>
+        <a href="#" class="news-item-cta">Read more >></a>
+    </div>
+</div>
+
+
+
+<div class="news-item">
+    <div class="news-item-img">
+        <img src="images/news1.jpg" alt="">
+    </div>
+    <div class="news-item-info">
+        <div class="news-item-date">
+            <span>Monday</span>
+            <span>September 20 2020</span>
+        </div>
+        <h1 class="news-item-title">Kanye Does It Again</h1>
+        <p class="news-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                  Illum consequuntur blanditiis hic sint adipisci.
+                                  Eveniet reiciendis a aliquam neque! Incidunt blanditiis atque pariatur minus quibusdam, voluptas omnis. Nulla, earum deleniti!
+        </p>
+        <a href="#" class="news-item-cta">Read more >></a>
+    </div>
+</div>
+
+
+<div class="news-item">
+    <div class="news-item-img">
+        <img src="images/news1.jpg" alt="">
+    </div>
+    <div class="news-item-info">
+        <div class="news-item-date">
+            <span>Monday</span>
+            <span>September 20 2020</span>
+        </div>
+        <h1 class="news-item-title">Kanye Does It Again</h1>
+        <p class="news-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                  Illum consequuntur blanditiis hic sint adipisci.
+                                  Eveniet reiciendis a aliquam neque! Incidunt blanditiis atque pariatur minus quibusdam, voluptas omnis. Nulla, earum deleniti!
+        </p>
+        <a href="#" class="news-item-cta">Read more >></a>
+    </div>
+</div>
+
+
+
+<div class="news-item">
+    <div class="news-item-img">
+        <img src="images/news1.jpg" alt="">
+    </div>
+    <div class="news-item-info">
+        <div class="news-item-date">
+            <span>Monday</span>
+            <span>September 20 2020</span>
+        </div>
+        <h1 class="news-item-title">Kanye Does It Again</h1>
+        <p class="news-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                  Illum consequuntur blanditiis hic sint adipisci.
+                                  Eveniet reiciendis a aliquam neque! Incidunt blanditiis atque pariatur minus quibusdam, voluptas omnis. Nulla, earum deleniti!
+        </p>
+        <a href="#" class="news-item-cta">Read more >></a>
+    </div>
+</div>
+
+
+<div class="news-item">
+    <div class="news-item-img">
+        <img src="images/news1.jpg" alt="">
+    </div>
+    <div class="news-item-info">
+        <div class="news-item-date">
+            <span>Monday</span>
+            <span>September 20 2020</span>
+        </div>
+        <h1 class="news-item-title">Kanye Does It Again</h1>
+        <p class="news-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                  Illum consequuntur blanditiis hic sint adipisci.
+                                  Eveniet reiciendis a aliquam neque! Incidunt blanditiis atque pariatur minus quibusdam, voluptas omnis. Nulla, earum deleniti!
+        </p>
+        <a href="#" class="news-item-cta">Read more >></a>
+    </div>
+</div>
+
+
+
+<div class="news-item">
+    <div class="news-item-img">
+        <img src="images/news1.jpg" alt="">
+    </div>
+    <div class="news-item-info">
+        <div class="news-item-date">
+            <span>Monday</span>
+            <span>September 20 2020</span>
+        </div>
+        <h1 class="news-item-title">Kanye Does It Again</h1>
+        <p class="news-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                  Illum consequuntur blanditiis hic sint adipisci.
+                                  Eveniet reiciendis a aliquam neque! Incidunt blanditiis atque pariatur minus quibusdam, voluptas omnis. Nulla, earum deleniti!
+        </p>
+        <a href="#" class="news-item-cta">Read more >></a>
+    </div>
+</div>
+
+
+<div class="news-item">
+    <div class="news-item-img">
+        <img src="images/news1.jpg" alt="">
+    </div>
+    <div class="news-item-info">
+        <div class="news-item-date">
+            <span>Monday</span>
+            <span>September 20 2020</span>
+        </div>
+        <h1 class="news-item-title">Kanye Does It Again</h1>
+        <p class="news-item-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                  Illum consequuntur blanditiis hic sint adipisci.
+                                  Eveniet reiciendis a aliquam neque! Incidunt blanditiis atque pariatur minus quibusdam, voluptas omnis. Nulla, earum deleniti!
+        </p>
+        <a href="#" class="news-item-cta">Read more >></a>
+    </div>
+</div>
+
+</div>
+    
+    
+    
+</body>
+</html>
