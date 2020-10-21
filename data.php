45cdec09ba3f2633e50c582fcb8dc570f5686a69
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2020-10-21 11:48:24 +0200

    Added changes:
    1. Scrapped the whole CSS-layout, will try different methods to see what works best going forward.
    2. Started building the array I will use for the exercise.
    3. Added the files: 'kladd.css' and 'kladd.html' where I will store old code for potential future use. Also added a logo-image, a new css file strictly for menu-styling, and an .editorconfig.-

---
+++ data.php
@@ -0,0 +1,41 @@
+<?php
+declare(strict_types=1);
+
+$newsitems = [
+    'title' => 'Kanye Does it again',
+    'author' => [
+        "ID" => 0,
+        "name" => 'Karl Jan',
+    ]
+    'date' => new DateTime("2020-09-12"),
+    'likes' => 1337,
+    'content' => [
+        'bla bla bla bla',
+        'hej jag heter orvar',
+        'hehehehehehe',
+    ],
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This is the file where you can keep your data arrays such as articles and
+// authors.
