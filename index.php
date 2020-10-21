1533de5ca1d05fe1b1e7c671adcae71f1a5d6fc3
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2020-10-21 14:01:45 +0200

    added:
    1. Banner image and a menu (subject to change or removal)

--- index.php
+++ index.php
@@ -9,6 +9,40 @@
     <title>Fake News</title>
 </head>
 <body>
+    <div class="banner">
+    <img src="images/logo.png">
+    </div>
+    <div class="topnav">
+    <a href="#home" class="active">Home</a>
+    <div id="myLinks">
+        <a href="#news">Sweden</a>
+        <a href="#contact">Global</a>
+        <a href="#about">Politics</a>
+        <a href="#news">Tech</a>
+        <a href="#contact">Business</a>
+        <a href="#about">Climate</a>
+        <a href="#contact">Arts</a>
+        <a href="#about">Entertainment</a>
+        <a href="#contact">Podcasts</a>
+        <a href="#about">Videos</a>
+  </div>
+
+<main>
+    <div>
+        <h2>Latest</h2>
+    </div>
+</main>
+    
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+</div>
+
+<div style="padding-left:16px">
+  <h3></h3>
+  <p></p>
+  <p></p>
+</div>
 
 
 <script src="script.js"></script>
