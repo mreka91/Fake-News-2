c505892603914388bd1cce904c4b91a454d8d432
Icarium2 <70521736+Icarium2@users.noreply.github.com>
2020-10-31 22:34:11 +0100

    Update data.php
    Added some more text to a few articles to make the cards look less empty.

--- data.php
+++ data.php
@@ -11,7 +11,7 @@ $newsItems = [
         "ID" => 0,
         "name" => "Mappo Trell",
         ],
-        "date" => new DateTime("2020-05-29"),
+        "date" => new DateTime("2020-12-30"),
         "likes" => 56356,
         "content" => 
             "After a year-long legal dispute between Facebook and the ECJ, the trial came to a shocking conclusion to say the least when Mark Zuckerberg arrived in Luxembourg without any lawyers. Claiming that the allegations were baseless, he, a genius, didn't need any assistance in defending his company. Zuckerberg commented that 'My company has been falsely accused by you huma- I mean people, and I intend to put these allegations to rest.' The trial began with EU Chief Prosecutor Laura Codruţa Kövese's opening statement, followed by Zuckerberg pleading not guilty to the allegations. The truly shocking turn of events came when Mr. Zuckerberg was put to the witness-stand where he froze and his eyes turned blank. Worried that Mr. Zuckerberg may have suffered a stroke, security rushed to help him, only to discover that he had spilled a glass of water on himself and thusly suffered a short-circuit. Mr. Zuckerberg is currently flying to Silicon Valley for repairs, and Facebook was sentenced to a fine of £500 million. Not thinking to waterproof Zuckerberg's circuits turned out to be a costly mistake, in the future he might want to consider not cheaping out when hiring engineers." 
@@ -31,7 +31,8 @@ $newsItems = [
         "likes" => 1234,
         "content" => 
             "The time is now for it to be okay to be great. People in this world shun people for being great. For being a bright color. For standing out. But the time is now to be okay to be the greatest you. Would you believe in what you believe in, if you were the only one who believed it?
-            As we live, our hearts turn colder. Cause pain is what we go through as we become older. We get insulted by others, lose trust for those others. We get back stabbed by friends. It becomes harder for us to give others a hand. We get our heart broken by people we love, even that we give them all we have. Then we lose family over time. What else could rust the heart more over time? Blackgold." 
+            As we live, our hearts turn colder. Cause pain is what we go through as we become older. We get insulted by others, lose trust for those others. We get back stabbed by friends. It becomes harder for us to give others a hand. We get our heart broken by people we love, even that we give them all we have. Then we lose family over time. What else could rust the heart more over time? Blackgold.
+            Society has put up so many boundaries, so many limitations on what’s right and wrong that it’s almost impossible to get a pure thought out. It’s like a little kid, a little boy, looking at colors, and no one told him what colors are good, before somebody tells you you shouldn’t like pink because that’s for girls, or you’d instantly become a gay two-year-old. Why would anyone pick blue over pink? Pink is obviously a better color. Everyone’s born confident, and everything’s taken away from you" 
 
     ],
 
@@ -47,7 +48,8 @@ $newsItems = [
         "date" => new DateTime("2020-11-14"),
         "likes" => 4321,
         "content" => 
-            "We’re not always in the position that we want to be at. We’re constantly growing. We’re constantly making mistakes. We’re constantly trying to express ourselves and actualize our dreams. If you have the opportunity to play this game of life you need to appreciate every moment. A lot of people don’t appreciate the moment until it’s passed." 
+            "We’re not always in the position that we want to be at. We’re constantly growing. We’re constantly making mistakes. We’re constantly trying to express ourselves and actualize our dreams. If you have the opportunity to play this game of life you need to appreciate every moment. A lot of people don’t appreciate the moment until it’s passed.
+            Society has put up so many boundaries, so many limitations on what’s right and wrong that it’s almost impossible to get a pure thought out. It’s like a little kid, a little boy, looking at colors, and no one told him what colors are good, before somebody tells you you shouldn’t like pink because that’s for girls, or you’d instantly become a gay two-year-old. Why would anyone pick blue over pink? Pink is obviously a better color. Everyone’s born confident, and everything’s taken away from you" 
 
     ],
 
@@ -63,14 +65,15 @@ $newsItems = [
         "date" => new DateTime("2020-11-19"),
         "likes" => 1122,
         "content" => 
-            "If everything I did failed - which it doesn't, it actually succeeds - just the fact that I'm willing to fail is an inspiration. People are so scared to lose that they don't even try. Like, one thing people can't say is that I'm not trying, and I'm not trying my hardest, and I'm not trying to do the best way I know how." 
+            "If everything I did failed - which it doesn't, it actually succeeds - just the fact that I'm willing to fail is an inspiration. People are so scared to lose that they don't even try. Like, one thing people can't say is that I'm not trying, and I'm not trying my hardest, and I'm not trying to do the best way I know how.
+            Society has put up so many boundaries, so many limitations on what’s right and wrong that it’s almost impossible to get a pure thought out. It’s like a little kid, a little boy, looking at colors, and no one told him what colors are good, before somebody tells you you shouldn’t like pink because that’s for girls, or you’d instantly become a gay two-year-old. Why would anyone pick blue over pink? Pink is obviously a better color. Everyone’s born confident, and everything’s taken away from you" 
 
     ],
 
 
     [
 
-        "title" => "Gates On The Pandemic: 'Yo, This Virus Is Lit Fam'",
+        "title" => "Gates On The Pandemic: 'BRUH, This Virus Is Lit Fam!!'",
         "image" => "https://medier.talentum.com/ponIltIpIv-1583129968/media/Nyteknik/s4omka-bill-gates.jpg/binary/original/bill-gates.jpg",
         "author" => [
         "ID" => 2,
@@ -79,7 +82,8 @@ $newsItems = [
         "date" => new DateTime("2020-12-01"),
         "likes" => 2211,
         "content" => 
-            "It really matters and then like it really doesn’t matter. What matters is the people who are sparked by it. And the people who are like offended by it, it doesn’t matter. Because it's about motivating the doers. Because I’m here to follow my dreams and inspire other people to follow their dreams, too." 
+            "It really matters and then like it really doesn’t matter. What matters is the people who are sparked by it. And the people who are like offended by it, it doesn’t matter. Because it's about motivating the doers. Because I’m here to follow my dreams and inspire other people to follow their dreams, too.
+            Society has put up so many boundaries, so many limitations on what’s right and wrong that it’s almost impossible to get a pure thought out. It’s like a little kid, a little boy, looking at colors, and no one told him what colors are good, before somebody tells you you shouldn’t like pink because that’s for girls, or you’d instantly become a gay two-year-old. Why would anyone pick blue over pink? Pink is obviously a better color. Everyone’s born confident, and everything’s taken away from you" 
 
     ],
 
@@ -111,7 +115,8 @@ $newsItems = [
         "date" => new DateTime("2020-02-01"),
         "likes" => 2233,
         "content" => 
-            "There’s nothing I really wanted to do in life that I wasn’t able to get good at. That’s my skill. I’m not really specifically talented at anything except for the ability to learn. That’s what I do. That’s what I’m here for. Don’t be afraid to be wrong because you can’t learn anything from a compliment." 
+            "There’s nothing I really wanted to do in life that I wasn’t able to get good at. That’s my skill. I’m not really specifically talented at anything except for the ability to learn. That’s what I do. That’s what I’m here for. Don’t be afraid to be wrong because you can’t learn anything from a compliment.
+            Society has put up so many boundaries, so many limitations on what’s right and wrong that it’s almost impossible to get a pure thought out. It’s like a little kid, a little boy, looking at colors, and no one told him what colors are good, before somebody tells you you shouldn’t like pink because that’s for girls, or you’d instantly become a gay two-year-old. Why would anyone pick blue over pink? Pink is obviously a better color. Everyone’s born confident, and everything’s taken away from you" 
 
     ],
 
@@ -129,7 +134,8 @@ $newsItems = [
         "content" => 
             "In that case, I'll tell you strait out. Say, why do you let those boys push you around like that? I haven't He's alright. Doc, you gotta help-
 
-            Shape up, man. You're a slacker. You wanna be a slacker for the rest of your life? Oh, what I meant to day was- Let's get him. George, buddy. remember that girl I introduced you to, Lorraine. What are you writing? Alright, good-bye Einy. Oh, watch that re-entry, it's a little bumpy." 
+            Shape up, man. You're a slacker. You wanna be a slacker for the rest of your life? Oh, what I meant to day was- Let's get him. George, buddy. remember that girl I introduced you to, Lorraine. What are you writing? Alright, good-bye Einy. Oh, watch that re-entry, it's a little bumpy. 
+            Society has put up so many boundaries, so many limitations on what’s right and wrong that it’s almost impossible to get a pure thought out. It’s like a little kid, a little boy, looking at colors, and no one told him what colors are good, before somebody tells you you shouldn’t like pink because that’s for girls, or you’d instantly become a gay two-year-old. Why would anyone pick blue over pink? Pink is obviously a better color. Everyone’s born confident, and everything’s taken away from you" 
 
     ],
 
